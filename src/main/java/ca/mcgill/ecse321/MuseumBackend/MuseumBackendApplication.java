a0dca328a0d811ff1c4ea24eef365c1b5df138f4
Coldstuff1 <samuel.faubert@mail.mcgill.ca>
2022-10-23 10:45:11 -0400

    sping_build_domain_model_1

---
+++ src/main/java/ca/mcgill/ecse321/MuseumBackend/MuseumBackendApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.MuseumBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseumBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/mcgill/ecse321/MuseumBackend/MuseumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.MuseumBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
