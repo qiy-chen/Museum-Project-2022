fdb3f03b060e0e0488aa929a6e4c71c9c6747d0e
jeanine56 <97963882+jeanine56@users.noreply.github.com>
2022-10-27 12:14:24 -0400

    Repository Tests

--- src/test/java/ca/mcgill/ecse321/MuseumBackend/repository/EmployeeRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/MuseumBackend/repository/EmployeeRepositoryTests.java
@@ -11,39 +11,60 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import ca.mcgill.ecse321.MuseumBackend.model.Employee;
-import ca.mcgill.ecse321.MuseumBackend.model.User;
+import ca.mcgill.ecse321.MuseumBackend.model.Person;
 
 @SpringBootTest
 public class EmployeeRepositoryTests {
 
 	@Autowired
 	private EmployeeRepository employeeRepository;
+	private PersonRepository personRepository;
 
 	@AfterEach
 	public void clearDatabase() {
 		employeeRepository.deleteAll();
+		personRepository.deleteAll();
 	}
-
+	
 	@Test
 	public void testPersistAndLoadEmployee() {
 		// Create object
-		String name = "anakin skywalker";
-		User aUser = new User();
 		Employee anakin = new Employee();
-		aUser.addUserRole(anakin);
-		aUser.setName(name);
 
 		// Save object
 		anakin = employeeRepository.save(anakin);
-		UUID id = anakin.getUserRoleId();
+		int id = anakin.getPersonRoleId();
 
 		// Read object from database
-		anakin = employeeRepository.findEmployeeByAUserRoleId(id);
+		anakin = employeeRepository.findEmployeeByPersonRoleId(id);
 
 		// Assert that object has correct attributes
 		assertNotNull(anakin);
-		assertEquals(id, anakin.getUserRoleId());
-		assertEquals(name, anakin.getUser(0).getName());
+		assertEquals(id, anakin.getPersonRoleId());
 	}
+	
+	@Test
+	public void testEmployeeToPersonReference() {
+		// Create object
+		String name = "anakin skywalker";
+		Person aPerson = new Person();
+		Employee anakin = new Employee();
+		anakin.setPerson(aPerson);
+		aPerson.setName(name);
+
+		// Save object
+		anakin = employeeRepository.save(anakin);
+		aPerson = personRepository.save(aPerson);
+		int id = anakin.getPersonRoleId();
+
+		// Read object from database
+		anakin = employeeRepository.findEmployeeByPersonRoleId(id);
+
+		// Assert that object has correct attributes
+		assertNotNull(anakin);
+		assertEquals(id, anakin.getPersonRoleId());
+		assertEquals(name, anakin.getPerson().getName());
+	}
+	
 	
 }
--- src/test/java/ca/mcgill/ecse321/MuseumBackend/repository/LoanRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/MuseumBackend/repository/LoanRepositoryTests.java
@@ -7,37 +7,64 @@ import java.util.UUID;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import ca.mcgill.ecse321.MuseumBackend.model.Artwork;
 import ca.mcgill.ecse321.MuseumBackend.model.Loan;
 
+
+@SpringBootTest
 public class LoanRepositoryTests {
 
 	@Autowired
 	private LoanRepository loanRepository;
+	private ArtworkRepository artworkRepository;
 
 	@AfterEach
 	public void clearDatabase() {
 		loanRepository.deleteAll();
+		artworkRepository.deleteAll();
 	}
 
 	@Test
 	public void testPersistAndLoadLoan() {
 		// Create object
 		Loan aLoan = new Loan();
-		UUID aLoanId = new UUID(0,0);
-		aLoan.setLoanId(aLoanId);
 
 		// Save object
 		loanRepository.save(aLoan);
-		aLoanId = aLoan.getLoanId();
+		int id = aLoan.getLoanId();
 
 		// Read object from database
-		aLoan = loanRepository.FindLoanById(aLoanId);
+		aLoan = loanRepository.FindLoanById(id);
 
 		// Assert that object has correct attributes
 		assertNotNull(aLoan);
-		assertEquals(aLoanId, aLoan.getLoanId());
+		assertEquals(id, aLoan.getLoanId());
+	}
+	
+	@Test
+	public void testLoanToArtworkReference() {
+		// Create object
+		Loan aLoan = new Loan();
+		Artwork art = new Artwork();
+		aLoan.setArtwork(art);
+
+		// Save object
+		art = artworkRepository.save(art);
+		aLoan = loanRepository.save(aLoan);
+		int id = aLoan.getLoanId();
+
+		// Read object from database
+		aLoan = loanRepository.FindLoanById(id);
+
+		// Assert that object has correct attributes
+		assertNotNull(aLoan);
+		assertEquals(id, aLoan.getLoanId());
+		assertEquals(art,aLoan.getArtwork());
 	}
 	
 }
