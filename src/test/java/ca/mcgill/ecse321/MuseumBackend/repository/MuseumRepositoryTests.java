217dd24e80dc98a425263f03c1028dd38e258312
alextsah <alexander.tsahageas@mail. mcgill.ca>
2022-10-27 13:08:41 -0400

    Updated Test Repositories

--- src/test/java/ca/mcgill/ecse321/MuseumBackend/repository/MuseumRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/MuseumBackend/repository/MuseumRepositoryTests.java
@@ -12,12 +12,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import ca.mcgill.ecse321.MuseumBackend.repository.MuseumRepository;
 
 import ca.mcgill.ecse321.MuseumBackend.model.Museum;
+import ca.mcgill.ecse321.MuseumBackend.model.Ticket;
 
 
 @SpringBootTest
 public class MuseumRepositoryTests {
     @Autowired
-    private MuseumRepository museumRepository;
+    private MuseumRepository ticketRepository;
+    
+    @Autowired
+    private TicketRepository museumRepository;
 
     @AfterEach
     public void clearDatabase() {
@@ -27,23 +31,36 @@ public class MuseumRepositoryTests {
     @Test
     public void testPersistAndLoadMuseum() {
       //Creating the museum instance 
-<<<<<<< Updated upstream
-      Museum museum = new Museum()
-=======
+
       int id = 123;
       Museum museum = new Museum(id);
->>>>>>> Stashed changes
+      
+      double price = 19.99;
+      int ticketId = 456;
+      Date date = new Date(2022, 04, 18);
+      
+      Ticket ticket = new Ticket (price, ticketId, date);
+      ticket.setMuseum(museum);
       
       //Save the museum instance in the MuseumRepository table 
       museumRepository.save(museum);
+      ticketRepository.save(ticket);
       
       //Obtain museumID of created museum object that was saved in MuseumRepository table 
       int museumID = museum.getMuseumId();
+      double ticketPrice = ticket.getPrice();
+      int ticketID = ticket.getTicketId();
+      Date ticketID = ticket.getTicketDate();
       
-      Museum museumDB = MuseumRepository.findMuseumByMuseumId(museumID);
+      int ticketIndex = museum.getTicketIndex(ticket);
+      museum = null;
+      
+      museum = MuseumRepository.findMuseumByMuseumId(museumID);
+      
       
       //Assertion Tests
-      assertNotNull(museumDB);
-      assertEquals(museumID, museumDB.getMuseumID());
+      assertNotNull(museum);
+      assertEquals(museumID, museum.getMuseumId);
+      assertEquals(ticket, museum.getTicket(ticketIndex));
     }
 }
--- src/test/java/ca/mcgill/ecse321/MuseumBackend/repository/StorageRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/MuseumBackend/repository/StorageRepositoryTests.java
@@ -2,11 +2,6 @@ package ca.mcgill.ecse321.MuseumBackend.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.AfterEach;
@@ -17,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import ca.mcgill.ecse321.MuseumBackend.repository.StorageRepository;
 
 import ca.mcgill.ecse321.MuseumBackend.model.Storage;
-import ca.mcgill.ecse321.MuseumBackend.model.Museum;
 import ca.mcgill.ecse321.MuseumBackend.model.Room;
 
 
@@ -30,54 +24,25 @@ public class StorageRepositoryTests {
 
     @AfterEach
     public void clearDatabase() {
-      museumRepository.deleteAll();
       storageRepository.deleteAll();
     }
     
     @Test
     public void testPersistAndLoadMuseum() {
         int roomNumber = 25;
-<<<<<<< Updated upstream
+        int roomId = 123;
         
-        Room storage = new Storage();
-        
-        storage.setRoomNumber(roomNumber);
-        int storageID = storage.getRoomId();
+        Room storage = new Storage(roomNumber, roomId);
         
         storageRepository.save(storage);
         
         storage = null;
         
         storage = storageRepository.findStorageByStorageId(storageID);
+        
         assertNotNull(storage);
-        assertEquals(storage.getRoomId(), storageID);
+        assertEquals(storage.getRoomId(), roomId);
         assertEquals(storage.getRoomNumber(), roomNumber);
-        
-       
-=======
-        int roomId = 123;
-        int museumID = 456;
-        
-        Museum museum = new Museum(museumID);
-        
-        Room storage = new Storage(roomNumber, roomId, museum);
-        
-        int storageID = storage.getRoomId();
-        int storageNumber = storage.getRoomNumber();
-        
-        storageRepository.save(storage);
-        museumRepository.save(museum);
-        
-        storage = null;
-        museum = null;
-        
-        storageDB = storageRepository.findStorageByStorageId(storageID);
-        museumDB = museumRepository.findMuseumByMuseumId(museumID);
-        
-        assertNotNull(storageDB);
-        assertEquals(storageDB.getRoomId(), storageID);
-        assertEquals(storageDB.getRoomNumber(), storageNumber);
-        assertEquals(storageDB.getMuseum(), museumDB);
->>>>>>> Stashed changes
+
     }
 }
