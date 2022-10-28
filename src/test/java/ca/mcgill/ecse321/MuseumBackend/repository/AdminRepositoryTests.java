0060e3336312d284000520a1d408a7613462e0ed
qiy-chen <qi.chen6@mail.mcgill.ca>
2022-10-28 12:10:29 -0400

    Readded other tests

---
+++ src/test/java/ca/mcgill/ecse321/MuseumBackend/repository/AdminRepositoryTests.java
@@ -0,0 +1,56 @@
+package ca.mcgill.ecse321.MuseumBackend.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.*; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.mcgill.ecse321.MuseumBackend.model.Admin;
+import ca.mcgill.ecse321.MuseumBackend.model.Person;
+import ca.mcgill.ecse321.MuseumBackend.model.Museum;
+import ca.mcgill.ecse321.MuseumBackend.model.PersonRole;
+import ca.mcgill.ecse321.MuseumBackend.model.Person;
+
+
+@SpringBootTest
+public class AdminRepositoryTests {
+	@Autowired
+	private AdminRepository adminRepository;
+
+	@Autowired
+	private MuseumRepository museumRepository;
+
+	@Autowired
+	private PersonRepository personRepository;
+	@AfterEach
+	public void clearDatabase() {
+		adminRepository.deleteAll();
+		museumRepository.deleteAll();
+		personRepository.deleteAll();
+	}
+
+	@Test
+	public void testPersistAndLoadAdmin() {
+		// Create object
+		String email = "samuel.faubert@mail.mcgill.ca";
+		String name = "Samuel Faubert";
+		String password = "MarwanisC00l";
+		Person samuel = new Person();
+		samuel.setEmail(email);
+		samuel.setName(name);
+		samuel.setPassword(password);
+		samuel = personRepository.save(samuel);
+		samuel.addPersonRole(new Admin());
+		samuel.getPersonRole(0).setPersonRoleId(324);
+		Admin anakin = (Admin) samuel.getPersonRole(0);
+		anakin = adminRepository.save(anakin);
+		int adminId = anakin.getPersonRoleId();
+		anakin = null;
+		anakin = adminRepository.findAdminByPersonRoleId(adminId);
+		assertNotNull(anakin);
+		assertEquals(adminId, anakin.getPersonRoleId());
+	}
+}
