f0bcba030c4ea127e5733d1be13cfba15560e6ba
Coldstuff1 <samuel.faubert@mail.mcgill.ca>
2022-10-28 17:14:35 -0400

    person_tests_fix

--- src/test/java/ca/mcgill/ecse321/MuseumBackend/repository/PersonRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/MuseumBackend/repository/PersonRepositoryTests.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.MuseumBackend.repository;
 
+import ca.mcgill.ecse321.MuseumBackend.model.Museum;
 import ca.mcgill.ecse321.MuseumBackend.model.Person;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,9 @@ public class PersonRepositoryTests {
     @Autowired
     private PersonRepository personRepository;
 
+    @Autowired
+    private MuseumRepository museumRepository;
+
 
     @AfterEach
     public void clearDatabase() {
@@ -26,6 +30,8 @@ public class PersonRepositoryTests {
     @Test
     public void testPersonPersistAndLoad() {
         // Create object
+        Museum museum = new Museum(12);
+        museum = museumRepository.save(museum);
         String email = "samuel.faubert@mail.mcgill.ca";
         String name = "Samuel Faubert";
         String password = "MarwanisC00l";
@@ -33,6 +39,7 @@ public class PersonRepositoryTests {
         samuel.setEmail(email);
         samuel.setName(name);
         samuel.setPassword(password);
+        samuel.setMuseum(museum);
         // Save object
         samuel = personRepository.save(samuel);
         String idEmail = samuel.getEmail();
