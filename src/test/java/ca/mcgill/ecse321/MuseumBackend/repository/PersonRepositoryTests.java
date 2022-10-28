2e2f83323d4ff1a772e7ea996770f7dc50daf62f
Coldstuff1 <samuel.faubert@mail.mcgill.ca>
2022-10-28 18:31:12 -0400

    person_associations_test

--- src/test/java/ca/mcgill/ecse321/MuseumBackend/repository/PersonRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/MuseumBackend/repository/PersonRepositoryTests.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.MuseumBackend.repository;
 
+import ca.mcgill.ecse321.MuseumBackend.model.Admin;
 import ca.mcgill.ecse321.MuseumBackend.model.Museum;
 import ca.mcgill.ecse321.MuseumBackend.model.Person;
 import org.junit.jupiter.api.AfterEach;
@@ -21,9 +22,13 @@ public class PersonRepositoryTests {
     @Autowired
     private MuseumRepository museumRepository;
 
+    @Autowired
+    private AdminRepository adminRepository;
+
 
     @AfterEach
     public void clearDatabase() {
+        adminRepository.deleteAll();
         personRepository.deleteAll();
         museumRepository.deleteAll();
     }
@@ -31,8 +36,6 @@ public class PersonRepositoryTests {
     @Test
     public void testPersonPersistAndLoad() {
         // Create object
-        Museum museum = new Museum(12);
-        museum = museumRepository.save(museum);
         String email = "samuel.faubert@mail.mcgill.ca";
         String name = "Samuel Faubert";
         String password = "MarwanisC00l";
@@ -40,9 +43,9 @@ public class PersonRepositoryTests {
         samuel.setEmail(email);
         samuel.setName(name);
         samuel.setPassword(password);
-        samuel.setMuseum(museum);
         // Save object
         samuel = personRepository.save(samuel);
+
         String idEmail = samuel.getEmail();
         samuel = null;
         // Read object from database
@@ -51,6 +54,27 @@ public class PersonRepositoryTests {
         assertEquals(idEmail, samuel.getEmail());
         assertEquals(name, samuel.getName());
         assertEquals(password, samuel.getPassword());
+
+    }
+    @Test
+    public void testPersonAssociations() {
+        Museum museum = new Museum(12);
+        museum = museumRepository.save(museum);
+        Admin admin = new Admin();
+        admin.setPersonRoleId(23);
+        admin = adminRepository.save(admin);
+        String email = "samuel.faubert@mail.mcgill.ca";
+        String name = "Samuel Faubert";
+        String password = "MarwanisC00l";
+        Person samuel = new Person();
+        samuel.setEmail(email);
+        samuel.setName(name);
+        samuel.setPassword(password);
+        samuel.setMuseum(museum);
+        admin.setPerson(samuel);
+        samuel = personRepository.save(samuel);
+        admin = adminRepository.save(admin);
+        String idEmail = samuel.getEmail();
 
     }
 }
