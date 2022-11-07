2bf97db5b60c9444d5b207b377db4d559f68acf0
alextsah <alexander.tsahageas@mail. mcgill.ca>
2022-11-07 16:07:42 -0500

    Update MuseumRepositoryTests.java

--- src/test/java/ca/mcgill/ecse321/MuseumBackend/repository/MuseumRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/MuseumBackend/repository/MuseumRepositoryTests.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
+import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
