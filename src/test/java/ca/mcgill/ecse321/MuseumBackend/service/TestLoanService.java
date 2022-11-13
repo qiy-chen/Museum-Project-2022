0a8488a966957569e04aac5e1922d015f96c1d21
alextsah <alexander.tsahageas@mail. mcgill.ca>
2022-11-12 19:19:33 -0500

    Update TestLoanService.java

--- src/test/java/ca/mcgill/ecse321/MuseumBackend/service/TestLoanService.java
+++ src/test/java/ca/mcgill/ecse321/MuseumBackend/service/TestLoanService.java
@@ -1,7 +1,9 @@
 package ca.mcgill.ecse321.MuseumBackend.service;
 
 
+import java.sql.Array;
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -67,6 +69,37 @@ public class TestLoanService {
     assertNotNull(loan1);
     assertEquals(LoanStatus.Returned, loan1.getStatus());
     assertTrue(loan1.getArtwork().getIsLoanable());
+  }
+  
+  @Test
+  public void testGetAllLoans() {
+    double rentalfee = 200.0;
+    Date startDate = new Date(0);
+    Date endDate = new Date(2);
+    int numOfDays = 20;
+    LoanStatus status = LoanStatus.Requested;
+    int loaniD = 1234;
+    Museum museum = new Museum();
+    Customer customer = new Customer();
+    Artwork artwork = new Artwork();
+    
+    Loan loan1 = new Loan(rentalfee,startDate,endDate,numOfDays,status,loaniD,museum,customer,artwork);
+    
+    int loaniD2 = 567;
+    Loan loan2 = new Loan(rentalfee,startDate,endDate,numOfDays,status,loaniD2,museum,customer,artwork);
+    
+    ArrayList<Loan> loans = new ArrayList<Loan>();
+    loans.add(loan1);
+    loans.add(loan2);
+    
+    when(loanRepository.findAll()).thenAnswer((InvocationOnMock invocation) -> loans);
+    
+    List<Loan> allloans = loanService.getAllLoans();
+    
+    assertNotNull(allloans);
+    assertEquals(loans,allloans);
+    assertEquals(loans.get(0),allloans.get(0));
+    assertEquals(loans.get(1),allloans.get(1));
   }
   
 }
