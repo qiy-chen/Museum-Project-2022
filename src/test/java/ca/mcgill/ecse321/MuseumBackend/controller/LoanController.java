82191e566f759c7f2b37ca8ed1c87aced84d35ff
emmakawczynski <emma.kawczynski@mail.mcgill.ca>
2022-11-14 17:53:13 -0500

    Controller

--- src/test/java/ca/mcgill/ecse321/MuseumBackend/controller/LoanController.java
+++ src/test/java/ca/mcgill/ecse321/MuseumBackend/controller/LoanController.java
@@ -1,5 +1,21 @@
 package ca.mcgill.ecse321.MuseumBackend.controller;
 
-public class LoanController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import ca.mcgill.ecse321.MuseumBackend.dto.LoanResponseDto;
+import ca.mcgill.ecse321.MuseumBackend.service.LoanService;
 
+@RestController
+public class LoanController {
+  @Autowired
+  LoanService loanService;
+  
+  @GetMapping
+  public ResponseEntity<LoanResponseDto> getLoanByLoanId(@PathVariable int Id){
+    
+  }
+  
 }
