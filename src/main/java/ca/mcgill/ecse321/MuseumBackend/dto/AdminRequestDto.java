863d37132d0e5842bd483fa9d6f3a4c1038e4145
Coldstuff1 <samuel.faubert@mail.mcgill.ca>
2022-11-19 15:22:36 -0500

    merge_fix

---
+++ src/main/java/ca/mcgill/ecse321/MuseumBackend/dto/AdminRequestDto.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse321.MuseumBackend.dto;
+
+public class AdminRequestDto {
+
+	// attributes
+	private String email;
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPersonEmail() {
+		return this.email;
+	}
+
+}
---
+++ src/main/java/ca/mcgill/ecse321/MuseumBackend/dto/AdminResponseDto.java
@@ -0,0 +1,44 @@
+package ca.mcgill.ecse321.MuseumBackend.dto;
+
+import java.util.List;
+
+import ca.mcgill.ecse321.MuseumBackend.model.Admin;
+
+public class AdminResponseDto {
+
+	private int id;
+	private PersonDto person;
+	private String email;
+	
+	public AdminResponseDto(Admin admin) {
+		this.id = admin.getPersonRoleId();
+		this.person = new PersonDto(admin.getPerson());
+		this.email = this.person.getEmail();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public PersonDto getPerson() {
+		return person;
+	}
+
+	public void setPerson(PersonDto person) {
+		this.person = person;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
