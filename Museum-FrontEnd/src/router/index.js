be68dd450b29e4e6cb5306eeb4c48d37975aa246
emmakawczynski <emma.kawczynski@mail.mcgill.ca>
2022-12-03 15:52:17 -0500

    added register.vue but doesn't work with router

--- Museum-FrontEnd/src/router/index.js
+++ Museum-FrontEnd/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Hello from '@/components/Hello'
 
 Vue.use(Router)
+//import register from './src/components/register.vue'
 
 export default new Router({
   routes: [
