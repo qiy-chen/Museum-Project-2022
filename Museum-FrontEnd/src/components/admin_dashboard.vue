16ce29fed54ed8c377a9a2800e9ee5e8a5e85a0a
jeanine56 <97963882+jeanine56@users.noreply.github.com>
2022-12-04 22:25:52 -0500

    The calendar works

--- Museum-FrontEnd/src/components/admin_dashboard.vue
+++ Museum-FrontEnd/src/components/admin_dashboard.vue
@@ -31,7 +31,7 @@
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Account</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#!">Go to Art Gallery</a></li>
+                                <li><a class="dropdown-item" href="#!">Go to Art Gallery</a></li>
                                     <li><a class="dropdown-item" href="file:///Users/alextsah/Documents/GitHub/project-group-15/Museum-Frontend/purchase_ticket.html">Buy Tickets</a></li>
                                     <li><a class="dropdown-item" href="request_loan.html">Loan Artwork</a></li>
                                     <li><a class="dropdown-item" href="index.html">Log out</a></li>
@@ -53,40 +53,64 @@
                         <h2 class="section-heading text-uppercase">Your Employees</h2>
                         <h3 class="section-subheading text-muted">These are all your currently registered employees</h3>
                     </div>
-                    <div class="container">
-                        <table class="styled-table">
+                        <div class="card-body">
+                        <vuejs-simple-dataTable :options="employeeTableOptions">
                             <thead>
                                 <tr>
-                                    <th>EmployeeId</th>
                                     <th>Name</th>
-                                    <th>Email</th>
+                                    <th>Position</th>
+                                    <th>Office</th>
+                                    <th>Age</th>
+                                    <th>Start date</th>
+                                    <th>Salary</th>
                                 </tr>
                             </thead>
+                            <tfoot>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Position</th>
+                                    <th>Office</th>
+                                    <th>Age</th>
+                                    <th>Start date</th>
+                                    <th>Salary</th>
+                                </tr>
+                            </tfoot>
                             <tbody>
-                                <!-- placeholder values to be removed when script is ready-->
-                                <tr class="active-row">
-                                    <td>#260972321</td> 
-                                    <td>Alex Tsahageas</td>
-                                    <td>alexa@mcgill.ca</td><button type="button">Fire Employee</button>
+                                <tr>
+                                    <td>Jonas Alexander</td>
+                                    <td>Developer</td>
+                                    <td>San Francisco</td>
+                                    <td>30</td>
+                                    <td>2010/07/14</td>
+                                    <td>$86,500</td>
                                 </tr>
                                 <tr>
-                                    <td>#260972321</td>
-                                    <td>Nick Tsahageas</td>
-                                    <td>alexa@mcgill.ca</td><button type="button">Fire Employee</button>
+                                    <td>Shad Decker</td>
+                                    <td>Regional Director</td>
+                                    <td>Edinburgh</td>
+                                    <td>51</td>
+                                    <td>2008/11/13</td>
+                                    <td>$183,000</td>
                                 </tr>
                                 <tr>
-                                    <td>#260972321</td>
-                                    <td>Andy Tsahageas</td>
-                                    <td>alexa@mcgill.ca</td><button type="button">Fire Employee</button>
+                                    <td>Michael Bruce</td>
+                                    <td>Javascript Developer</td>
+                                    <td>Singapore</td>
+                                    <td>29</td>
+                                    <td>2011/06/27</td>
+                                    <td>$183,000</td>
                                 </tr>
                                 <tr>
-                                    <td>#260972321</td>
-                                    <td>Sandy Tsahageas</td>
-                                    <td>alexa@mcgill.ca</td><button type="button">Fire Employee</button>
+                                    <td>Donna Snider</td>
+                                    <td>Customer Support</td>
+                                    <td>New York</td>
+                                    <td>27</td>
+                                    <td>2011/01/25</td>
+                                    <td>$112,000</td>
                                 </tr>
                             </tbody>
-                        </table>
-                    </div>
+                        </vuejs-simple-dataTable>
+                        </div>
                 </div> 
             </section>
             <div class="text-center">
@@ -110,14 +134,31 @@
                     </div>
                 </div>
             </section>
+
+            <!-- schedule viewer -->
+            <section class="page-section" id="scheduleView">
+                <div class="container">
+                    <div class="text-center">
+                        <h2 class="section-heading text-uppercase">Your Schedule</h2>
+                    <full-calendar class="test-fc" :events="fcEvents" 
+                    first-day='1' locale="fr"
+                    @changeMonth="changeMonth"
+                    @eventClick="eventClick"
+                    @dayClick="dayClick"
+                    @moreClick="moreClick">
+                        <template slot="fc-event-card" scope="p">
+                            <p><i class="fa">sadfsd</i> {{ p.event.title }} test</p>
+                        </template>
+                    </full-calendar>
+                    </div>
+                </div>
+            </section>
+
             <footer class="footer py-4">
                 <div class="container">
                     <div class="row align-items-center">
                         <div class="col-lg-4 text-lg-start">Copyright &copy; Museum Website 2022</div>
-                        <div class="col-lg-4 my-3 my-lg-0">
-                            <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="GitHub"><i class="fab fa-github"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Email"><i class="fab fa-facebook-f"></i></a>
-                        </div>
+                        <div class="col-lg-4 my-3 my-lg-0"></div>
                         <div class="col-lg-4 text-lg-end">
                             <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
                             <a class="link-dark text-decoration-none" href="#!">Terms of Use</a>
@@ -131,9 +172,86 @@
     src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" 
     crossorigin="anonymous"
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    src="src/scripts.js"
+    src="src/assets/scripts.js"
     src="https://cdn.startbootstrap.com/sb-forms-latest.js"
+
+    
+    var demoEvents = [
+        {
+            title    : 'Sunny 725-727',
+            start    : '2017-02-25',
+            end      : '2017-02-27',
+            cssClass : 'family'
+        },
+        {
+            title : 'Lunfel 726-727',
+            start : '2017-02-26',
+            end : '2017-02-27',
+            cssClass : ['home', 'work']
+        },
+        {
+            title : 'Lunfel 2/27-2/28',
+            start : '2017-02-27',
+            end : '2017-02-28'
+        },
+        {
+            title : 'Lunfel 2/27-2/28',
+            start : '2017-02-27',
+            end : '2017-02-28'
+        },
+        {
+            title : 'Lunfel 2/27-2/28',
+            start : '2017-02-27',
+            end : '2017-02-28'
+        },
+        {
+            title : 'Lunfel 2/26-3/05',
+            start : '2017-02-26',
+            end : '2017-03-05'
+        },
+        {
+            title : 'Lunfel 1/27-1/28',
+            start : '2017-01-27',
+            end : '2017-01-28'
+        },
+        {
+            title : 'Lunfel 1/27-2/2',
+            start : '2017-01-27',
+            end : '2017-02-02'
+        },
+        {
+            title : 'Lunfel 3/27-3/28',
+            start : '2017-03-27',
+            end : '2017-03-28'
+        },
+    ];
+    export default {
+        data () {
+            return {
+                name:'Sunny!',
+                fcEvents : demoEvents
+            }
+        },
+        methods : {
+            'changeMonth' (start, end, current) {
+            console.log('changeMonth', start.format(), end.format(), current.format())
+            },
+            'eventClick' (event, jsEvent, pos) {
+            console.log('eventClick', event, jsEvent, pos)
+            },
+            'dayClick' (day, jsEvent) {
+            console.log('dayClick', day, jsEvent)
+            },
+            'moreClick' (day, events, jsEvent) {
+            console.log('moreCLick', day, events, jsEvent)
+            }
+        },
+        components : {
+            'full-calendar': require('vue-fullcalendar')	
+        }
+    }
 </script>
+
 <style>
     @import '*/Museum-FrontEnd/scr/styles.css';
 </style>
